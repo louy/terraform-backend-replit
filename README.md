736737767bca00cd778ea28c479e30929cac84ca
Louay Alakkad <louay_alakkad@mckinsey.com>
2020-11-02 17:30:21 +0000

    Add first implementation

--- README.md
+++ README.md
@@ -1,9 +1,51 @@
 # Terraform Backend: Repl.it
 
-A [terraform backend](https://www.terraform.io/docs/backends/types/http.html) implementation using repl.it. Requires a Paid account to get a private repl.
+A [terraform backend](https://www.terraform.io/docs/backends/types/http.html) implementation using repl.it. Can be used with free accounts as well.
 
 ## So what's this?
 
-This repo contains a [repl.it "repl"](https://repl.it/site/about) that can be used as a remote state backend for terraform. The advantage of storing this in something like repl.it instead of AWS S3 is that it's much easier to set up, and some of my hobby projects tend to use repl.it anyway.
+This repo contains a [repl.it "repl"](https://repl.it/site/about) that can be used as a remote state backend for terraform. The advantage of storing this in something like repl.it instead of AWS S3 is that it's much easier to set up, and some of my hobby projects tend to use repl.it anyway. Plus it's free!
 
 This backend supports state locks and an having arbitrary number of terraform states on a single worker (using different pathnames).
+
+## Getting started
+
+To use this as a backend, all you need is to open the public repl for this repo and fork it, then change the secret values.
+
+### Secret values
+
+To use this backend, you'll need to set a private username and password. These can be pretty much anything. The server would launch until you set them, so what you need to do is create a file in your repl.it called `.env` and add the following:
+
+```
+USERNAME=changeme
+PASSWORD=changeme
+```
+
+According to [the docs](https://docs.repl.it/repls/secret-keys) your `.env` file is private, even when you use a public repl, but you can always use a private repl if you have a paid account.
+
+Congrats! Once you've saved your changes you're good to go!
+
+Use the replit preview url as your backend url in your terraform config. Check out the `example` folder on how to set that up. It'll look something like this:
+
+```hcl
+terraform {
+  backend "http" {
+    address = "https://terraform-backend.ACCOUNT_NAME.repl.it/"
+    username = "CHANGE ME!"
+    password = "CHANGE ME!"
+  }
+}
+```
+
+**Caution:** Changing your credentials after running `terraform init` is not supported as it's not straightforward. If that's needed, try taking a copy of your state before changing your credentials, then uploading it after you make the change:
+
+```sh
+# Before changing your credentials
+tf state pull > state-backup.tfstate
+
+# Change your credentials on repl.it and save
+# ...
+
+# After changing your credentials (including in the terraform config)
+tf state push state-backup.tfstate
+```
---
+++ index.js
@@ -0,0 +1,195 @@
+// These options should map to the tf backend configuration:
+// https://www.terraform.io/docs/backends/types/http.html
+const USERNAME = process.env.TF_BACKEND_USERNAME;
+const PASSWORD = process.env.TF_BACKEND_PASSWORD;
+
+const UPDATE_METHOD = 'POST';
+const LOCK_ENDPOINT_SUFFIX = '';
+const LOCK_METHOD = 'LOCK';
+const UNLOCK_ENDPOINT_SUFFIX = '';
+const UNLOCK_METHOD = 'UNLOCK';
+
+// /TF Backend options
+
+const STATE_KEY_PREFIX = 'state::';
+const LOCK_KEY_PREFIX = 'lock::';
+
+// Refuse to launch if not configured properly
+if (!USERNAME || !PASSWORD) {
+  throw new Error(
+    "Please set a username and a password in the repl's .env file. Check the readme for more info",
+  );
+}
+
+function btoa(string) {
+  return Buffer.from(string).toString('base64');
+}
+
+class Response {
+  constructor(body = '', options = {}) {
+    this.body = body;
+    this.headers = options.headers || {};
+    this.status = options.status || 200;
+  }
+}
+
+const Client = require('@replit/database');
+const client = new Client();
+
+const http = require('http');
+const server = http.createServer(async (req, res) => {
+  let response;
+  try {
+    req.text = () =>
+      new Promise((resolve, reject) => {
+        let body = [];
+        req
+          .on('data', chunk => {
+            body.push(chunk);
+          })
+          .on('end', () => {
+            resolve(Buffer.concat(body).toString());
+          })
+          .on('error', reject);
+      });
+
+    req.headers.get = header => req.headers[header.toLowerCase()];
+
+    response = await handleRequest(req);
+  } catch (error) {
+    response = new Response(error.stack, { status: 500 });
+  }
+
+  res.writeHead(response.status, response.headers).end(response.body);
+});
+server.listen(80);
+
+async function handleRequest(req) {
+  try {
+    {
+      // Check authorisation
+      let authError = await authenticate(req);
+      if (authError) return authError;
+    }
+
+    let requestURL = new URL(req.url);
+    const path = requestURL.pathname;
+    switch (true) {
+      case req.method === 'GET':
+        return await getState(path);
+      case req.method === UPDATE_METHOD:
+        return await setState(path, await req.text());
+      case req.method === 'DELETE':
+        return await deleteState(path);
+      case req.method === LOCK_METHOD && path.endsWith(LOCK_ENDPOINT_SUFFIX):
+        return await lockState(
+          path.substr(0, path.length - LOCK_ENDPOINT_SUFFIX.length),
+          await req.text(),
+        );
+      case req.method === UNLOCK_METHOD &&
+        path.endsWith(UNLOCK_ENDPOINT_SUFFIX):
+        return await unlockState(
+          path.substr(0, path.length - UNLOCK_ENDPOINT_SUFFIX.length),
+          await req.text(),
+        );
+    }
+
+    return new Response('Nothing found at ' + requestURL.pathname, {
+      status: 404,
+    });
+  } catch (error) {
+    return new Response(error.stack, { status: 500 });
+  }
+}
+
+const expectedToken = btoa([USERNAME, PASSWORD].join(':'));
+async function authenticate(req) {
+  const authHeader = req.headers.get('Authorization');
+  if (!authHeader || typeof authHeader !== 'string') {
+    return new Response('Missing credentials', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Terraform State"',
+      },
+    });
+  }
+
+  const [scheme, credentials, ...rest] = authHeader.split(' ');
+  if (rest.length != 0 || scheme !== 'Basic' || credentials !== expectedToken) {
+    return new Response('Invalid credentials', {
+      status: 403,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Terraform State"',
+      },
+    });
+  }
+
+  return void 0;
+}
+
+async function getState(path) {
+  const state = await client.get(STATE_KEY_PREFIX + path);
+  if (!state) {
+    return new Response('', {
+      status: 404,
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    });
+  }
+
+  return new Response(state || '', {
+    headers: {
+      'Content-type': 'application/json',
+      'Cache-Control': 'no-store',
+    },
+  });
+}
+async function setState(path, body) {
+  await client.set(STATE_KEY_PREFIX + path, body);
+  return new Response(body || '', {
+    status: 200,
+    headers: {
+      'Content-type': 'application/json',
+      'Cache-Control': 'no-store',
+    },
+  });
+}
+async function deleteState(path) {
+  await client.delete(STATE_KEY_PREFIX + path);
+  return new Response('', {
+    status: 200,
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  });
+}
+
+async function lockState(path, body) {
+  const existingLock = await client.get(LOCK_KEY_PREFIX + path);
+  if (existingLock) {
+    return new Response(existingLock, {
+      status: 423,
+      headers: {
+        'Content-type': 'application/json',
+        'Cache-Control': 'no-store',
+      },
+    });
+  }
+  await client.set(LOCK_KEY_PREFIX + path, body);
+  return new Response(body, {
+    headers: {
+      'Content-type': 'application/json',
+      'Cache-Control': 'no-store',
+    },
+  });
+}
+
+async function unlockState(path, body) {
+  await client.delete(LOCK_KEY_PREFIX + path);
+  return new Response('', {
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  });
+}
